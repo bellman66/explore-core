a74b21250a3a078a9c4a0abe6aadfd8d114d21ae
bellman66 <rndso15@naver.com>
2025-02-01 21:02:48 +0900

    feat: x86 irq code

---
+++ os/linux/interrupt/irq.c
@@ -0,0 +1,12 @@
+/**
+*   interrupt 발생시 사용되는 irq 호출 메소드
+*   do_irq 값이 수정되어 handle_irq 메소드로 변경됨
+*/
+static __always_inline void handle_irq(struct irq_desc *desc,
+				       struct pt_regs *regs)
+{
+	if (IS_ENABLED(CONFIG_X86_64))
+		generic_handle_irq_desc(desc);
+	else
+		__handle_irq(desc, regs);
+}
