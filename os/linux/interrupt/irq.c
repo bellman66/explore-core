e5f60b109efa766d109f51787fe82edd226417e6
bellman66 <rndso15@naver.com>
2025-02-02 21:37:26 +0900

    chore: memo 업데이트

--- os/linux/interrupt/irq.c
+++ os/linux/interrupt/irq.c
@@ -1,6 +1,7 @@
-/**
-*   interrupt 발생시 사용되는 irq 호출 메소드
-*   do_irq 값이 수정되어 handle_irq 메소드로 변경됨
+/*
+* interrupt 발생시 사용되는 irq 호출 메소드
+* do_irq 값이 수정되어 handle_irq 메소드로 변경
+* path : x86/kernel/irq.c
 */
 static __always_inline void handle_irq(struct irq_desc *desc,
 				       struct pt_regs *regs)
